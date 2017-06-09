9646661bc1652970085a150f900f0b2244620047
jiangzhiguo <jiangzhiguo@qianshengqian.com>
2017-06-09 13:37:30 +0800

    生成豆子

--- src/js/function.js
+++ src/js/function.js
@@ -3,42 +3,12 @@
  */
 
 /**
- * 构建画布
- * @param x x-axis
- * @param y y-axis
- * @param canvas 画布
- */
-function init_canvas(x, y, canvas) {
-    var canvas_cont = ''
-    for (var i = 0; i < y; i++) {
-        //构建豆池
-        beans_pond.beans_pond_y.push(i + 1)
-        var axis_x = x
-        for (var j = 0; j < axis_x; j++) {
-            var point = 'x' + j + 'y' + i
-            canvas_cont += '<div class="size" id="' + point + '"></div>'
-        }
-    }
-
-    //构建豆池
-    for (var j = 0; j < x; j++) {
-        beans_pond.beans_pond_x.push(j + 1)
-    }
-    beans_pond.update_pond()
-
-    canvas.innerHTML = canvas_cont
-}
-
-/**
  * 控制类
- * @type {{}}
+ * @type {{snake_body: [*], run_flag: null, cur_direction: string, next_direction: string, sleep_time: number, direction_set: [*], allow_set: [*], move_up: Control_direction.move_up, move_down: Control_direction.move_down, move_left: Control_direction.move_left, move_right: Control_direction.move_right, run: Control_direction.run, start: Control_direction.start, pause: Control_direction.pause, end: Control_direction.end}}
  */
 var Control_direction = {
     //构建蛇身体
     snake_body: [{x: 5, y: 9}, {x: 5, y: 10}, {x: 5, y: 11}],
-
-    //清除的身体
-    remove_body: null,
 
     //运行标志
     run_flag: null,
@@ -62,15 +32,14 @@ var Control_direction = {
         var axis_x = this.snake_body[0].x
         if (axis_y < 0) {
             this.end()
-            return
+            return false
         }
 
         var elem = {x: axis_x, y: axis_y}
-        var last = this.snake_body.pop()
-        Show.remove_class(last)
         this.snake_body.unshift(elem)
+        Show.remove_class(this.snake_body.pop())
         Show.add_class(elem)
-        this.remove_body = last
+        return true
     },
 
     //下方向
@@ -79,14 +48,14 @@ var Control_direction = {
         var axis_x = this.snake_body[0].x
         if (axis_y >= canvas_y) {
             this.end()
-            return
+            return false
         }
+
         var elem = {x: axis_x, y: axis_y}
-        var last = this.snake_body.pop()
-        Show.remove_class(last)
         this.snake_body.unshift(elem)
+        Show.remove_class(this.snake_body.pop())
         Show.add_class(elem)
-        this.remove_body = last
+        return true
     },
 
     //左方向
@@ -95,15 +64,14 @@ var Control_direction = {
         var axis_x = this.snake_body[0].x - 1
         if (axis_x < 0) {
             this.end()
-            return
+            return false
         }
 
         var elem = {x: axis_x, y: axis_y}
-        var last = this.snake_body.pop()
-        Show.remove_class(last)
         this.snake_body.unshift(elem)
+        Show.remove_class(this.snake_body.pop())
         Show.add_class(elem)
-        this.remove_body = last
+        return true
     },
 
     //右方向
@@ -112,22 +80,21 @@ var Control_direction = {
         var axis_x = this.snake_body[0].x + 1
         if (axis_x >= canvas_x) {
             this.end()
-            return
+            return false
         }
 
         var elem = {x: axis_x, y: axis_y}
-        var last = this.snake_body.pop()
-        Show.remove_class(last)
         this.snake_body.unshift(elem)
+        Show.remove_class(this.snake_body.pop())
         Show.add_class(elem)
-        this.remove_body = last
+        return true
     },
     run: function () {
 
     },
     start: function () {
         move_snake()
-        this.run_flag = setInterval("move_snake()", this.sleep_time)
+        // this.run_flag = setInterval("move_snake()", this.sleep_time)
     },
     pause: function () {
         clearInterval(directionObj.run_flag)
@@ -146,42 +113,98 @@ var Control_direction = {
  * @type {{fill_canvas: Show.fill_canvas, add_class: Show.add_class}}
  */
 var Show = {
-    fill_canvas: function (body) {
+    //蛇身样式
+    body_css: 'fill_box',
+
+    //豆子样式
+    beans_css: 'beans',
+
+    //填充画布
+    fill_canvas: function (body, class_name) {
+        if (!class_name) class_name = this.body_css;
+
         for (index in body) {
-            this.add_class(body[index])
+            this.add_class(body[index], class_name)
         }
     },
-    add_class: function (body) {
+
+    //显示
+    add_class: function (body, class_name) {
+        if (!class_name) class_name = this.body_css;
         var point = 'x' + body.x + 'y' + body.y
         var origin_class = $(point).getAttribute('class')
-        var class_name = 'fill_box'
         if (origin_class != null) {
             class_name = origin_class + ' ' + class_name
         }
+
         $(point).setAttribute('class', class_name)
     },
+
+    //隐藏
     remove_class: function (body) {
         var point = 'x' + body.x + 'y' + body.y
         $(point).setAttribute('class', 'size')
     }
 }
 
+/**
+ * 生成豆子
+ * @type {{beans_pond_x: Array, beans_pond_y: Array, beans_point: Array, update_pond: Generate_beans.update_pond, init_pond: Generate_beans.init_pond, create_beans: Generate_beans.create_beans}}
+ */
 var Generate_beans = {
+    //豆池
     beans_pond_x: [],
     beans_pond_y: [],
+
+    //豆子所在位置
+    beans_point: [],
+
+    //更新豆池
     update_pond: function () {
+        this.init_pond()
         for (index in directionObj.snake_body) {
-            this.beans_pond_x.splice(directionObj.snake_body[index].x, 1)
-            this.beans_pond_y.splice(directionObj.snake_body[index].y, 1)
+            var point_x = directionObj.snake_body[index].x
+            var point_y = directionObj.snake_body[index].y
+
+            this.beans_pond_x[point_x].body = 1
+            this.beans_pond_y[point_y].body = 1
         }
 
-        //蛇身尾迹加入豆池
-        if (this.remove_body != null) {
-            for (index in directionObj.remove_body)
-            this.beans_pond_y.push(directionObj.remove_body[index].y)
-            this.beans_pond_x.push(directionObj.remove_body[index].x)
+        var rang_x = []
+        for (index in this.beans_pond_x) {
+            if (this.beans_pond_x[index].body != 1) {
+                rang_x.push(index)
+            }
         }
 
+        var rang_y = []
+        for (index in this.beans_pond_y) {
+            if (this.beans_pond_y[index].body != 1) {
+                rang_y.push(index)
+            }
+        }
+
+        this.create_beans(rang_x, rang_y)
+    },
+    //清除豆池
+    init_pond: function () {
+        for (index in this.beans_pond_x) {
+            this.beans_pond_x[index].body = 0
+        }
+
+        for (index in this.beans_pond_y) {
+            this.beans_pond_y[index].body = 0
+        }
+
+    },
+    //创建豆子
+    create_beans: function (rang_x, rang_y) {
+
+        var point_x = rang_x[Math.floor(Math.random() * rang_x.length)] * 1
+        var point_y = rang_y[Math.floor(Math.random() * rang_y.length)] * 1
+
+        this.beans_point.push({x: point_x, y: point_y})
+        Show.add_class(this.beans_point[0], Show.beans_css)
     }
 }
 
@@ -208,28 +231,55 @@ function is_exist(object, index) {
     return false
 }
 
+/**
+ * 单步移动
+ * @returns {boolean}
+ */
 function move_snake() {
 
+    var move_result = false
     switch (directionObj.next_direction) {
         case 'ArrowUp':
-            directionObj.move_up()
+            move_result = directionObj.move_up()
             break;
         case 'ArrowDown':
-            directionObj.move_down()
+            move_result = directionObj.move_down()
             break;
         case 'ArrowLeft':
-            directionObj.move_left()
+            move_result = directionObj.move_left()
             break;
         case 'ArrowRight':
-            directionObj.move_right()
+            move_result = directionObj.move_right()
             break;
         default:
             break;
     }
 
-    //更新豆池
-    beans_pond.update_pond()
-    console.log(beans_pond.beans_pond_y)
-    console.log(beans_pond.beans_pond_x)
+    return move_result
+}
 
+/**
+ * 构建画布
+ * @param x x-axis
+ * @param y y-axis
+ * @param canvas 画布
+ */
+function init_canvas(x, y, canvas) {
+    var canvas_cont = ''
+    for (var i = 0; i < y; i++) {
+        //构建豆池
+        beans_pond.beans_pond_y.push({body: 0})
+        var axis_x = x
+        for (var j = 0; j < axis_x; j++) {
+            var point = 'x' + j + 'y' + i
+            canvas_cont += '<div class="size" id="' + point + '"></div>'
+        }
+    }
+
+    //构建豆池
+    for (var j = 0; j < x; j++) {
+        beans_pond.beans_pond_x.push({body: 0})
+    }
+    canvas.innerHTML = canvas_cont
+    beans_pond.update_pond()
 }
